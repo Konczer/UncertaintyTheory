882fcec8f7f0dbef4bcebc4ae2cc14204fb48e3a
josi <konczer.j@gmail.com>
2024-05-27 17:50:23 +0200

    internal _binomial_fishergame_solve added

--- StatisticalGames/Software/Python/statgames/statgames/fisher_game.py
+++ StatisticalGames/Software/Python/statgames/statgames/fisher_game.py
@@ -32,6 +32,7 @@ def fishergame_solve(N: int, KA: int, KB: int, M: int) -> Dict[str, Union[int, f
     Jozsef Konczer, "Statistical Games",  arXiv:2402.15892, 2024.
     Available at: https://arxiv.org/abs/2402.15892
     """
+
     # Check if all parameters are integers and non-negative
     if not all(isinstance(param, int) for param in [N, KA, KB, M]):
         raise TypeError("All parameters must be integers")
@@ -49,7 +50,7 @@ def fishergame_solve(N: int, KA: int, KB: int, M: int) -> Dict[str, Union[int, f
     # Return the output of the internal _fishergame_solve   
     return _fishergame_solve(N, KA, KB, M)
 
-def _fishergame_solve(N, KA, KB, M):
+def _fishergame_solve(N: int, KA: int, KB: int, M: int) -> Dict[str, Union[int, float]]:
     """
     Internal function to calculate Fisher game equilibrium quantities.
 
@@ -81,6 +82,7 @@ def _fishergame_solve(N, KA, KB, M):
             return 0
         return scipy.special.comb(KB, k, exact=True) * scipy.special.comb(M - KB, N - k, exact=True) 
 
+
     z_AB=[0, 0]
     for k in range(k_minmax[0], k_minmax[1] + 1):
 
@@ -102,6 +104,62 @@ def _fishergame_solve(N, KA, KB, M):
     P_star = w_A_star / (w_A_star + w_B_star)
     nu_star = (Z_B + w_B_star - Z_A) / (w_A_star + w_B_star)
     v_star = (Z_A + w_A_star * nu_star) / Z
+    s_star = (k_star + nu_star) / (N + 1)
+
+    return {'P':P_star, 'k':k_star, 'nu':nu_star, 's':s_star, 'v':v_star}
+
+
+def _binomial_fishergame_solve(N: int, xA: float, xB: float) -> Dict[str, Union[int, float]]:
+    """
+    Internal function to calculate Binomial Fisher game equilibrium quantities.
+
+    Parameters:
+    N (int): Number of sampled bits.
+    xA (float): Fraction of 1-s in case of scenario A.
+    xB (float): Fraction of 1-s in case of scenario B.
+
+    Returns:
+    dict: Contains 'P', 'k', 'nu', 's', 'v'.
+    """
+    
+    def p_k_A(k):
+        if k == 0:
+            return (1 - xA) ** N
+        elif k == N:
+            return xA ** N
+        else:
+            return scipy.special.comb(N, k, exact=True) * (xA ** k) * ((1 - xA) ** (N - k))
+
+    def p_k_B(k):
+        if k == 0:
+            return (1 - xB) ** N
+        elif k == N:
+            return xB ** N
+        else:
+            return scipy.special.comb(N, k, exact=True) * (xB ** k) * ((1 - xB) ** (N - k))
+
+
+    s_AB=[0, 0]
+    for k in range(0, N + 1):
+
+        s_AB[0] += p_k_A(k)
+        s_AB[1] += p_k_B(k)
+        
+        if sum(s_AB) > 1:
+            k_star = k
+            s_AB_star = s_AB
+            break
+    
+
+    p_A_star = p_k_A(k_star)
+    p_B_star = p_k_B(k_star)
+
+    Sigma_A = s_AB_star[0] - p_A_star
+    Sigma_B = 1 - s_AB_star[1]
+
+    P_star = p_A_star / (p_A_star + p_B_star)
+    nu_star = (Sigma_B + p_B_star - Sigma_A) / (p_A_star + p_B_star)
+    v_star = Sigma_A + p_A_star * nu_star
     s_star = (k_star + nu_star) / (N + 1)
 
     return {'P':P_star, 'k':k_star, 'nu':nu_star, 's':s_star, 'v':v_star}
